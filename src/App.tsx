15dfea796637af09e8b882b23af2287cfd58ccc3
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-04-16 18:44:57 -0300

    now in app it is called the landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import LandingPage from './pages/Home';
+import LandingPage from './pages/Home';
 
 function App() {
   return (
     <>
-    {/* <LandingPage/> */}
+    <LandingPage/>
     </>
   );
 }
