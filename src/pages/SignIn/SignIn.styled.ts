fb0c5aff01d322e4e01212223c743c183b9ab2c1
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-04-21 20:23:22 -0300

    pre-created singin pager

---
+++ src/pages/SignIn/SignIn.styled.ts
@@ -0,0 +1,60 @@
+import styled from 'styled-components'
+
+
+export const Container = styled.div`
+  height: 100vh;
+
+  img {
+    height: 100vh;
+
+    @media (max-width: 940px) {
+      display: none;
+    }
+  }
+`
+
+export const Content = styled.div`
+  position: absolute;
+  left: 52%;
+  top: 27%;
+  max-width: 345px;
+
+  a {
+    color: var(--default-black);
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    display: block;
+    max-width: 345px;
+    margin: 30px 0;
+  }
+
+  
+
+  @media (max-width: 940px) {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+`
+
+export const Password = styled.div`
+  button {
+    background: none;
+    border: 0;
+    bottom: 0;
+    position: relative;
+    left: 320px;
+    transition: 1s;
+    top: -37px;
+    width: 30px;
+    color: var(--default-black);
+    opacity: 45%;
+
+    svg {
+      margin: -7px 0 0 -30px;
+      position: relative;
+    }
+  }
+`
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,102 @@
+import { useEffect, useLayoutEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import { FiEye } from 'react-icons/fi'
+
+import { TextField } from '../../ui/components/TextField'
+import { login } from '../../assets'
+
+import * as S from './SignIn.styled'
+import user from '../../service/user/user'
+import { token } from '../../service/api'
+
+type FormData = {
+  email: string
+  password: string
+}
+
+export default function SignIn() {
+
+  const [ textPass, setTextPass ] = useState(true)
+
+  // const navigate = useNavigate()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>()
+
+  async function onSubmit(data: FormData) {
+    console.log(data)
+
+    const canLogin = await user.login(data.email, data.password)
+
+    console.log("canLogin")
+    console.log(canLogin)
+
+    if(!canLogin) return
+    
+    window.location.pathname = '/admin'
+
+  }
+
+  function checkIfIsLogged(){
+    // if(token) window.location.pathname = '/dashboard'
+  }
+
+  useLayoutEffect(
+    () => {
+      checkIfIsLogged()
+    },[]
+  )
+
+  return (
+    <S.Container>
+      <img src={login} alt='image sign in' />
+
+      <S.Content>
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <TextField
+            label='E-mail'
+            id='email'
+            type='email'
+            
+            errorMessage={errors.email?.message}
+
+            placeholder='examplo@gmail.com'
+            {...register('email', {
+              required: {
+                value: true,
+                message: 'Todos os campos são obrigatórios',
+              },
+            })}
+          />
+
+          <S.Password>
+
+            <TextField
+              label='Senha'
+              id='password'
+              placeholder='sua melhor senha'
+              type={textPass ? 'password' : 'text'}
+              {...register('password', {
+                required: true,
+              })}
+              
+            />
+
+            <button onClick={() => setTextPass(!textPass)} type='button'>
+              <FiEye size={20} />
+            </button>
+          </S.Password>
+          <button type='submit' >
+            Login
+          </button>
+        </form>
+        <Link to='/cadastro'>Cadastre-se</Link>
+      </S.Content>
+    </S.Container>
+  )
+}
