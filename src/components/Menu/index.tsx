070b4c7d40d5c7f0c33ae99d3dc82148924a6a47
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-04-21 20:09:47 -0300

    updated menu component

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,46 +4,28 @@ import './nav.css'
 
 import './nav-responsive.css'
 import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Menu = (
   // { imageLink, imageSrc }: { imageLink: string, imageSrc: string }
-  ) => {
+) => {
 
-    const [isMenuActive, setIsMenuActive] = useState(false);
-  
+  const [isMenuActive, setIsMenuActive] = useState(false);
 
-  const navSlide = () => {
-    const burger = document.querySelector(".burger")
-    const nav = document.querySelector(".responsive-navbar__nav-list")
-    const liLinks = document.querySelectorAll("responsive-navbar__nav-list__list-el")
 
-        //@ts-ignore
-        burger.addEventListener('click', () => {
-            //Toggle Nav
-              //@ts-ignore
-            nav.classList.toggle('nav-list--active')
-            //Animate Links    
-            liLinks.forEach((link, index) => {
-                //@ts-ignore
-                if(link.style.animation){
-                          //@ts-ignore
-                    link.style.animation = ``
-                }
-                else{
-                          //@ts-ignore
-                    link.style.animation = `nav-link--fade  0.5s ease fowards ${index/ 7 + 1.5}s`
-                }
-                })
-    })
-  }
-  /*
-  Quando o hamburger é clicado 
-    ele adiciona a classe
-      ativa no ul
-      faz um map adicionado as animações dinâmicamente
+  let socialMedias: any[] = [
+    { 
+      href:  'https://www.facebook.com/Barbearia-do-Parra-108829248169948/',
+      icon:  'facebook'
+    },
+    { 
+      href:  'https://www.instagram.com/barbeariadoparraa/',
+      icon:  'instagram'
+    },
+    
+  ]
 
-  */
-  let pageComponents=[
+  let pageComponents = [
     {
       href: '#about',
       title: 'Sobre'
@@ -67,47 +49,58 @@ const Menu = (
           </h2>
           <nav className="responsive-navbar">
             <ul className="responsive-navbar__nav-list"
-            style={
-              {
-                transform:  isMenuActive ? `translateX(0%)` : `translateX(100%)` ,
+              style={
+                {
+                  transform: isMenuActive ? `translateX(0%)` : `translateX(100%)`,
+                }
               }
-            }
             >
               <li className="responsive-navbar__nav-list__list-el">
                 <a href="https://api.whatsapp.com/send?phone=+5515997702196" className="list-el__link">Agendar</a>
               </li>
               <li className="responsive-navbar__nav-list__list-el--social-links">
-                <a href="https://www.facebook.com/Barbearia-do-Parra-108829248169948/" target="_blank" className="header-page__anchor"><span className="fab fab--white fa-facebook-f"></span> </a>
-                <a href="https://www.instagram.com/barbeariadoparraa/" target="_blank" className="header-page__anchor"><span className="fab fab--white fa-instagram"></span></a>
+                {
+                  socialMedias.map(
+                    socialMedia => (
+                    <a href={socialMedia.icon} target="_blank" className="header-page__anchor">
+                      <span className={socialMedia.icon}>
+                      </span>
+                      <FontAwesomeIcon icon={["fab", socialMedia.icon]} />
+                    </a>
+                    )
+                  )
+                }
+
+
               </li>
               {
-              pageComponents.map(
-                (component) => (
-                  <li
-                  className="responsive-navbar__nav-list__list-el">
-                    <a
-                    href={component.href}
-                    className="list-el__link">
-                      {component.title}
-                    </a>
-                  </li>
+                pageComponents.map(
+                  (component) => (
+                    <li
+                      className="responsive-navbar__nav-list__list-el">
+                      <a
+                        href={component.href}
+                        className="list-el__link">
+                        {component.title}
+                      </a>
+                    </li>
                   )
-              )
+                )
               }
             </ul>
           </nav>
-            <div className="burger"
+          <div className="burger"
             onClick={(e) => {
               setIsMenuActive(!isMenuActive)
             }}
-            >
-              <div className="burger__line1"></div>
-              <div className="burger__line2"></div>
-              <div className="burger__line3"></div>
-            </div>
+          >
+            <div className="burger__line1"></div>
+            <div className="burger__line2"></div>
+            <div className="burger__line3"></div>
+          </div>
         </div>
       </div>
-      
+
     </>
   );
 }
