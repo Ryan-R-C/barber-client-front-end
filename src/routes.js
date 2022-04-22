cd38e90c1a5753cadefa9ecc816377a4d51eff6c
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-04-21 21:47:24 -0300

    added the route of signup

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './pages/Home'
 import Admin from './pages/Admin'
 import SignIn from './pages/SignIn'
+import SignUp from "./pages/SignUp";
 
 
 const RoutesApp = () => {
@@ -10,9 +11,11 @@ const RoutesApp = () => {
     <>
     <BrowserRouter>
       <Routes>
-        <Route path='/'                    element={<Home />}/>
-        <Route path='/SignIn'               element={<SignIn />}/>
-        <Route path='/admin'               element={<Admin />}/>
+        <Route path='/'                    element={<Home />}    />
+        <Route path='/admin'               element={<Admin />}   />
+        <Route path='/signin'              element={<SignIn />}  />
+        <Route path='/cadastro'            element={<SignUp />}  />
+
       </Routes>
     </BrowserRouter>
     </>
