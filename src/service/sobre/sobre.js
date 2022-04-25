2b93c89c7a94926f5aed72152917f1b1332176a0
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-04-25 18:48:20 -0300

    added about service

---
+++ src/service/sobre/sobre.js
@@ -0,0 +1,111 @@
+import { api, apiWithoutTenant } from '../api'
+
+import responseHandler from '../../utils/responseHandler'
+import servidorErrorMessage from '../../utils/servidorErrorMessage'
+export default class sobreService {
+  static async create(data) {
+    // console.log()
+    const response = await api
+      .post('sobre', {
+        data,
+      })
+
+      console.log(response)
+
+      .catch(() => {
+        servidorErrorMessage()
+      })
+
+    const mensagemOk = 'Área criado com sucesso!'
+    const mensagemNaoOK = 'Revise seus dados :('
+    responseHandler(response.status, mensagemOk, mensagemNaoOK)
+
+    const responseData = response.data
+    return responseData
+  }
+
+  //= =========================================================================================================
+  static async update(id, data) {
+    const response = await api
+      .put(`sobre/${id}`, {
+        data,
+      })
+      .catch(() => {
+        servidorErrorMessage()
+      })
+    const mensagemOk = 'Área alterado com sucesso!'
+    const mensagemNaoOK = 'Revise seus dados :('
+    responseHandler(response.status, mensagemOk, mensagemNaoOK)
+
+    const responseData = response.data
+
+    return responseData
+  }
+
+  //= =========================================================================================================
+  static async delete(id) {
+    const response = await api
+      .delete(`sobre/${id}`)
+      .then((res) => {
+        const status = res.status
+        const mensagemOk = 'Modulo apagado com sucesso!'
+        const mensagemNaoOK = 'Algo deu errado :('
+
+        responseHandler(status, mensagemOk, mensagemNaoOK)
+      })
+
+      .catch(() => {
+        servidorErrorMessage()
+      })
+
+    return response
+  }
+
+  //= =========================================================================================================
+  static async list() {
+    const response = await apiWithoutTenant.get('sobre').catch(() => {
+      servidorErrorMessage()
+    })
+    console.log("response")
+    console.log(response)
+
+    const responseData = response.data.rows
+
+    return responseData
+  }
+
+  static async listWithFilter(filter, value) {
+    const response = await api
+      .get(`sobre?filter%5B${filter}%5D=${value}`)
+      .catch(() => {
+        servidorErrorMessage()
+      })
+
+    const responseData = response.data.rows
+
+    return responseData
+  }
+
+  //= =========================================================================================================
+  static async listWithManyFilters(filters) {
+    const response = await api.get(`sobre?${filters}`).catch(() => {
+      servidorErrorMessage()
+    })
+
+    const responseData = response.data.rows
+
+    return responseData
+  }
+
+  //= =========================================================================================================
+  static async find(id) {
+    const response = await api
+      .get(`sobre/${id}`)
+
+      .catch(() => {
+        servidorErrorMessage()
+      })
+
+    return response.data
+  }
+}
